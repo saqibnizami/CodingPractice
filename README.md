5d7e8937f576a9a11f8394c6feeb8f6db7ce3c5d
Saqib Nizami <snizami@gmail.com>
2019-09-12 15:31:35 -0400

    first solution

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coding Practice
+---
+Various practice problems from around the web. Organized by language and source.
---
+++ SQL/HackerRank/RevisingTheSelectQueryI.sql
@@ -0,0 +1,8 @@
+-- Revising the Select Query I
+-- https://www.hackerrank.com/challenges/revising-the-select-query/problem
+-- lang: mysql
+
+SELECT *
+FROM CITY
+WHERE POPULATION > 100000
+HAVING COUNTRYCODE = 'USA'
