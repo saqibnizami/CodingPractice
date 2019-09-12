1de7c5133bc1fa099905d734063ad63c1ba0a2d7
Saqib Nizami <snizami@gmail.com>
2019-09-12 15:36:51 -0400

    update 2

---
+++ SQL/HackerRank/RevisingTheSelectQueryII.sql
@@ -0,0 +1,12 @@
+-- Revising the Select Query II
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+-- lang: mysql
+-- promt: Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
+
+SELECT NAME  
+FROM (
+    SELECT *
+    FROM CITY
+    WHERE POPULATION > 120000
+    HAVING COUNTRYCODE = 'USA'
+) as USCITIES
